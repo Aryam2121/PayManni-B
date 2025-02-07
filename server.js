2bf62dde34979ac7f604ae5ccd96f7c2028be41c
Aryam2121 <aryamangupta2121@gmail.com>
2025-02-07 19:50:30 +0530

    add in auth

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const passport = require("passport");
 
-// Importing Routes
+dotenv.config();
+
+const app = express();
+
+require("./config/passport");
+
 const userRoutes = require("./Routes/userRoutes");
 const RechargeRoutes = require("./Routes/RechargeRoutes");
 const WalletRoutes = require("./Routes/WalletRoutes");
 const contactRoutes = require("./Routes/ContactRoutes");
 const transactionRoutes = require("./Routes/transactionRoutes");
 
-dotenv.config();
-const app = express();
-
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Database Connection
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET ,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose
   .connect(process.env.MONGO_URI, {
     dbName: "PayManni", // Optional: Replace with your database name if needed
   })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// Routes
-app.use("/api/users", userRoutes);
+app.use("/api/auth", userRoutes);
 app.use("/api/recharge", RechargeRoutes);
 app.use("/api", WalletRoutes);
 app.use("/api", contactRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
