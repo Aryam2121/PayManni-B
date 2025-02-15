6719454b94da05881bf1d853bed3b1d0dd159b6e
Aryam2121 <aryamangupta2121@gmail.com>
2025-02-15 18:29:53 +0530

    adding Wallet.js model

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,27 +1,27 @@
-// models/walletModel.js
-let wallet = {
-  balance: 5000,
-  transactions: [],
+const mongoose = require("mongoose");
+
+const walletSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  balance: { type: Number, default: 0 },
+  transactions: [
+    {
+      amount: Number,
+      type: { type: String, enum: ["Deposit", "Withdraw", "Payment"], required: true },
+      timestamp: { type: Date, default: Date.now },
+    },
+  ],
+});
+
+walletSchema.methods.updateBalance = async function (amount) {
+  this.balance += amount;
+  return this.save();
 };
 
-const getWallet = () => wallet;
-
-const updateBalance = (amount) => {
-  wallet.balance += amount;
+walletSchema.methods.addTransaction = async function (amount, type) {
+  this.transactions.push({ amount, type });
+  return this.save();
 };
 
-const addTransaction = (amount, type) => {
-  const transaction = {
-    id: wallet.transactions.length + 1,
-    amount,
-    type,
-    date: new Date().toLocaleString(),
-  };
-  wallet.transactions.push(transaction);
-};
+const Wallet = mongoose.model("Wallet", walletSchema);
 
-module.exports = {
-  getWallet,
-  updateBalance,
-  addTransaction,
-};
+module.exports = Wallet;
