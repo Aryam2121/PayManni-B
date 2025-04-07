acc7dc99aa2b6c1e12606eef98ebe6c8ddd78d5d
Aryam2121 <aryamangupta2121@gmail.com>
2025-04-07 18:23:07 +0530

    add upi id generation

---
+++ models/Userupi.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  balance: { type: Number, default: 0 },
+  upiId: { type: String, unique: true, sparse: true }, // e.g., john@paymanni
+});
+
+const Userupi = mongoose.model('Userupi', userSchema);
+module.exports = Userupi;
