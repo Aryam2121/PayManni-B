e22eb2acdb0e06d5055c5782d065050071d9c5b7
Aryam2121 <aryamangupta2121@gmail.com>
2025-04-10 12:51:01 +0530

    update userroutes

--- Controllers/UserupiController.js
+++ Controllers/UserupiController.js
@@ -1,5 +1,39 @@
 const Userupi = require("../models/Userupi");
+const getUserById = async (req, res) => {
+  const { userId } = req.params;
 
+  try {
+    const user = await Userupi.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    res.status(200).json(user); // send full user object
+  } catch (err) {
+    res.status(500).json({ msg: "Server error", err });
+  }
+};
+const getUserBankData = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await Userupi.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+
+    // Simulated banking data response
+    res.json({
+      linkedAccounts: user.linkedAccounts || [],
+      transactions: user.transactions || [],
+      virtualUpiId: user.virtualUpiId || `${user.name}@paymanni`,
+    });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error", err });
+  }
+};
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -23,5 +57,24 @@ const registerUser = async (req, res) => {
     res.status(500).json({ msg: "Server error", err });
   }
 };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { registerUser };
+  try {
+    const user = await Userupi.findOne({ email });
+
+    if (!user || user.password !== password) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    res.status(200).json({ 
+      msg: "Login successful", 
+      userId: user._id, // <-- this is what you use in frontend
+      user
+    });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error", err });
+  }
+};
+
+module.exports = { registerUser, getUserBankData,getUserById, loginUser };
