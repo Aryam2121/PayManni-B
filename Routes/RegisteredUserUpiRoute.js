70232e1446e60a0e86daa4a3799fd8ad7f200d8c
Aryam2121 <aryamangupta2121@gmail.com>
2025-04-11 13:32:36 +0530

    add getMyaccountdetails api

--- Routes/RegisteredUserUpiRoute.js
+++ Routes/RegisteredUserUpiRoute.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, getUserById,
     getUserBankData,
-    loginUser} = require("../Controllers/UserupiController");
+    loginUser,getMyAccountDetails} = require("../Controllers/UserupiController");
 
 router.post("/register", registerUser);
 router.get('/:userId', getUserById); 
 router.get('/:userId/bank-data', getUserBankData); 
 router.post('/login', loginUser); // Login route
-
+router.get("/myaccount/:userId", getMyAccountDetails);
 module.exports = router;
