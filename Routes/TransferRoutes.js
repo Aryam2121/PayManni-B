a5556e81ea41d0bf43c5b0369e2df45923bf1499
Aryam2121 <aryamangupta2121@gmail.com>
2025-04-17 10:20:23 +0530

    update transfer routes

--- Routes/TransferRoutes.js
+++ Routes/TransferRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const transferController = require("../Controllers/TransferController.js");
-const { authenticateUser } = require("../Middleware/authMiddleware.js"); // ✅ Destructure the function
 
 // ✅ Create a Transfer (with Razorpay)
-router.post("/transfer", authenticateUser, transferController.createTransfer); // ✅ Correct middleware usage
+router.post("/transfer",transferController.createTransfer); // ✅ Correct middleware usage
 
 // ✅ Get User Transfers
-router.get("/transfers", authenticateUser, transferController.getTransfers); // ✅ Correct middleware usage
+router.get("/transfers",transferController.getTransfers); // ✅ Correct middleware usage
 
 module.exports = router;
