3c1cf5352884bc6c4868d5726b8401a0b47ebbb8
Aryam2121 <aryamangupta2121@gmail.com>
2025-05-01 22:26:25 +0530

    update firebase credentials

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-service-account.json"); // Download this from Firebase Console
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
